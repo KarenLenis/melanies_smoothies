adb72b73fc58d3a05509aa1be1adb468cb5031b2
Karen Lenis <102916080+KarenLenis@users.noreply.github.com>
2025-09-19 13:08:20 +0200

    Update streamlit_app.py
    ..

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,13 +40,12 @@ if ingredients_list:
       #          values('"""+ ingredients_string+"""','"""+ name_on_order+"""')"""
     #st.write(my_insert_stmt)
     #st.stop()
-      
-      time_to_insert=st.button('Submit Order')
+time_to_insert=st.button('Submit Order')
       
       
 #Insert the Order into Snowflake
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+if time_to_insert:
+      session.sql(my_insert_stmt).collect()
+      st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
 #Add a Submit Button
