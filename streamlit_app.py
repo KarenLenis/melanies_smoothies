8771bb9e85b13aecd04ea697e3e766663aaeba15
Karen Lenis <102916080+KarenLenis@users.noreply.github.com>
2025-09-19 14:02:57 +0200

    Update streamlit_app.py
    ..

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,8 @@ if ingredients_list:
       sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 #Build a SQL Insert Statement & Test It
-      #my_insert_stmt="""insert into smoothies.public.orders(ingredients,name_on_order)
-      #          values('"""+ ingredients_string+"""','"""+ name_on_order+"""')"""
+my_insert_stmt="""insert into smoothies.public.orders(ingredients,name_on_order)
+                values('"""+ ingredients_string+"""','"""+ name_on_order+"""')"""
     #st.write(my_insert_stmt)
     #st.stop()
 time_to_insert=st.button('Submit Order')
