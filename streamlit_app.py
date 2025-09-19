3520ba6408cd70d9fa98224cd2c9afbc93c4b6c9
Karen Lenis <102916080+KarenLenis@users.noreply.github.com>
2025-09-19 12:53:10 +0200

    Update streamlit_app.py
    Add line request for API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 
@@ -36,7 +37,8 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     #st.stop()
     time_to_insert=st.button('Submit Order')
-
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 #Insert the Order into Snowflake
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
